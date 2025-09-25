850f322ba1ad6abb415a8db7aa8af139cdab155a
ssahaatwork <ssahaatwork@gmail.com>
2025-09-25 18:53:03 +0000

    fix header nav link active state

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+// Default configuration for local development
+const config = {
+  site: 'http://localhost:4321',
+  base: '/',
+};
+
+// Update configuration for GitHub Pages deployment
+if (process.env.DEPLOY_TARGET === 'gh-pages') {
+  config.site = 'https://atssj.github.io';
+  config.base = '/agency-website';
+}
+
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://atssj.github.io',
-    base: '/agency-website',
-});
+export default defineConfig(config);
